fe4e25856d3f8d27e56b69cf3f0712defe9bf960
ericguillaumet <eguillaumet@protonmail.com>
2023-01-14 13:10:49 +0800

    Updating CSS, creating templates base and Index

--- Register_IE/routes.py
+++ Register_IE/routes.py
@@ -3,4 +3,9 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template("index.html", pageTitle="All")
+    movement_data=[
+        {"id": 1, "date": "2023-01-01", "concept":"Salary", "quantity": 1500},
+        {"id": 2, "date": "2023-01-05", "concept":"Cake", "quantity": -10},
+        {"id": 3, "date": "2023-01-06", "concept":"Clothes", "quantity": -40},
+    ]
+    return render_template("index.html", pageTitle="All", data=movement_data)
